54b6cfe7f91581dc19c85f727ab296a438ff2908
Juan Pedro Fisanotti <fisadev@gmail.com>
2015-02-12 02:19:30 -0300

    Added initial version of data downloader

---
+++ download_data.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+"""
+Script to download the raw data from http://www.rsssf.com/
+The data was processed mostly by interactive sessions in ipython. Almost every
+file had it's own format, so there is no point in trying to automate it in a
+fully automatic script, but this downloading script may be useful for future
+dowloads.
+"""
+import requests
+
+YEAR_URL = 'http://www.rsssf.com/tablesa/arg%i.html'
+FILE_PATH = 'data/%i.txt'
+
+for year in range(90, 2016):
+    print 'Year:', year
+    try:
+        data = requests.get(YEAR_URL % year).content
+        with open(FILE_PATH % year, 'w') as data_file:
+            data_file.write(data)
+        print 'Wrote file with', len(data), 'chars'
+    except:
+        print 'ERROR!'
