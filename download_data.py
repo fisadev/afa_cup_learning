958a134bd5e68ac94e3f4f25d4587e357d9a7ba5
Juan Pedro Fisanotti <fisadev@gmail.com>
2015-02-12 02:25:30 -0300

    Use nltk to extract text from html data

--- download_data.py
+++ download_data.py
@@ -6,6 +6,7 @@ file had it's own format, so there is no point in trying to automate it in a
 fully automatic script, but this downloading script may be useful for future
 dowloads.
 """
+import nltk
 import requests
 
 YEAR_URL = 'http://www.rsssf.com/tablesa/arg%i.html'
@@ -15,9 +16,10 @@ YEARS = range(90, 100) + range(2000, 2016)
 for year in YEARS:
     print 'Year:', year
     try:
-        data = requests.get(YEAR_URL % year).content
+        html = requests.get(YEAR_URL % year).content
+        text = nltk.clean_html(html)
         with open(FILE_PATH % year, 'w') as data_file:
-            data_file.write(data)
-        print 'Wrote file with', len(data), 'chars'
+            data_file.write(text)
+        print 'Wrote file with', len(text), 'chars'
     except:
         print 'ERROR!'
