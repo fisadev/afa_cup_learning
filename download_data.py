3c9820088931d76503694f0224856d2424233e09
Juan Pedro Fisanotti <fisadev@gmail.com>
2015-02-12 02:37:21 -0300

    Use html2text instead of nltk, remove error handling, and added encoding handling

--- download_data.py
+++ download_data.py
@@ -6,7 +6,7 @@ file had it's own format, so there is no point in trying to automate it in a
 fully automatic script, but this downloading script may be useful for future
 dowloads.
 """
-import nltk
+from html2text import html2text
 import requests
 
 YEAR_URL = 'http://www.rsssf.com/tablesa/arg%i.html'
@@ -15,11 +15,11 @@ YEARS = range(90, 100) + range(2000, 2016)
 
 for year in YEARS:
     print 'Year:', year
-    try:
-        html = requests.get(YEAR_URL % year).content
-        text = nltk.clean_html(html)
-        with open(FILE_PATH % year, 'w') as data_file:
-            data_file.write(text)
-        print 'Wrote file with', len(text), 'chars'
-    except:
-        print 'ERROR!'
+    data = requests.get(YEAR_URL % year).content
+    data = data.decode('windows-1252')
+    data = data.replace('\r\n', '\n')
+    data = html2text(data)
+
+    with open(FILE_PATH % year, 'w') as data_file:
+        data_file.write(data.encode('utf-8'))
+    print 'Wrote file with', len(data), 'chars'
